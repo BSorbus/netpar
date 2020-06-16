ab6f7ce5498348b91c87ad502f5261c497b86332
BSorbus <BSorbus@gmail.com>
2020-06-16 08:49:14 +0200

    Boom

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -305,6 +305,6 @@ Devise.setup do |config|
   # config.captcha_for_confirmation = true
 
   # Time period for account expiry from last_activity_at
-  config.expire_after = 90.days
+  config.expire_after = 6.months
 
 end
