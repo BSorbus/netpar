569e30c4e598dcfd487a566477ee93220493f255
BSorbus <BSorbus@gmail.com>
2021-11-18 13:47:19 +0100

    Integrated with TestPortal part 17

--- app/controllers/api/v1/testportal_results_controller.rb
+++ app/controllers/api/v1/testportal_results_controller.rb
@@ -27,7 +27,7 @@ class Api::V1::TestportalResultsController < Api::V1::BaseApiController
         # if grade.update(testportal_params)
 
      puts '-----------------------------------------'
-        default = (params[:testResult][:formattedPercents].to_i >= 60) ? { grade_result: "T" } : { grade_result: "N" }  
+     puts   default = (params[:testResult][:percents] >= 60) ? { grade_result: "T" } : { grade_result: "N" }  
      puts '-----------------------------------------'
         puts default
         if grade.update(testportal_params)
@@ -44,7 +44,7 @@ class Api::V1::TestportalResultsController < Api::V1::BaseApiController
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def testportal_params
-      defaults = (params[:testResult][:formattedPercents].to_i >= 60) ? { grade_result: "T" } : { grade_result: "N" }  
+      defaults = (params[:testResult][:percents] >= 60) ? { grade_result: "T" } : { grade_result: "N" }  
       params.require(:testResult).permit().reverse_merge(defaults)
     end
 
