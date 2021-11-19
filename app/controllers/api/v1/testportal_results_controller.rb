d8bd764f98aa7f457b4bb9512e6aca2a98cbbcf5
BSorbus <BSorbus@gmail.com>
2021-11-19 10:06:55 +0100

    Integrated with TestPortal part 18

--- app/controllers/api/v1/testportal_results_controller.rb
+++ app/controllers/api/v1/testportal_results_controller.rb
@@ -7,29 +7,17 @@ class Api::V1::TestportalResultsController < Api::V1::BaseApiController
 
 
   def create
-    # params[:proposal] = JSON.parse params[:proposal].gsub('=>', ':')
-    # puts '-----------------------------------------'
-    # puts params
-    # # puts params[:testportal_results]
-    # # puts params[:testportal_results]
-    # # puts params[:testportal_results]
-    # puts params[:testResult]
-    # puts params[:testResult][:accessCode]
-    # puts '.........................................'
-    # puts testportal_params
-    # puts '-----------------------------------------'
-
-    unless params[:testResult][:formattedPercents].present?
-      render json: { errors: "'formattedPercents' parameter is missing" }, status: :not_acceptable
+    unless params[:testResult][:percents].present?
+      render json: { errors: "'percents' parameter is missing" }, status: :not_acceptable
     else
       grade = Grade.find_by(testportal_access_code_id: params[:testResult][:accessCode])
       if grade.present?
         # if grade.update(testportal_params)
 
-     puts '-----------------------------------------'
-     puts   default = (params[:testResult][:percents] >= 60) ? { grade_result: "T" } : { grade_result: "N" }  
-     puts '-----------------------------------------'
-        puts default
+         puts '-----------------------------------------'
+         puts   testportal_params 
+         puts '-----------------------------------------'
+
         if grade.update(testportal_params)
             render json: { message: "Result saved" }, status: :ok
         else
@@ -44,8 +32,8 @@ class Api::V1::TestportalResultsController < Api::V1::BaseApiController
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def testportal_params
-      defaults = (params[:testResult][:percents] >= 60) ? { grade_result: "T" } : { grade_result: "N" }  
-      params.require(:testResult).permit().reverse_merge(defaults)
+      defaults = (params[:testResult][:percents] >= 60) ? { grade_result: "P" } : { grade_result: "N" }  
+      params.require(:testResult).permit(:grade_result).reverse_merge(defaults)
     end
 
 
