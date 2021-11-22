54c0f3e903e21fc7f9b419f6033355e63799eec9
BSorbus <BSorbus@gmail.com>
2021-11-22 12:34:05 +0100

    Integrated with TestPortal part 19

--- app/serializers/api/v1/grade_serializer.rb
+++ app/serializers/api/v1/grade_serializer.rb
@@ -3,8 +3,14 @@ class Api::V1::GradeSerializer < ActiveModel::Serializer
   attributes :id, :examination_id, :subject_id, :grade_result, :testportal_access_code_id, :subject_name, :testportal_test_info
 
   def testportal_test_info
+    # data = {}
+    # id_test = ExamsDivisionsSubject.joins(:exams_division, exams_division: [:exam]).where(subject: object.subject, exams_divisions: {division_id: object.examination.division_id} ).first.testportal_test_id
+    # api_call_correct, test_hash = ApiTestportalTest::test_info_in_testportal_where_test_id("#{id_test}")
+    # if api_call_correct
+    #   data = test_hash unless test_hash.blank?
+    # end 
     data = {}
-    id_test = ExamsDivisionsSubject.joins(:exams_division, exams_division: [:exam]).where(subject: object.subject, exams_divisions: {division_id: object.examination.division_id} ).first.testportal_test_id
+    id_test = ExamsDivisionsSubject.joins(:exams_division, exams_division: [:exam]).where(subject_id: object.subject_id, exams_divisions: {division_id: object.examination.division_id} ).uniq.first.testportal_test_id
     api_call_correct, test_hash = ApiTestportalTest::test_info_in_testportal_where_test_id("#{id_test}")
     if api_call_correct
       data = test_hash unless test_hash.blank?
