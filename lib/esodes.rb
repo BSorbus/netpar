fed197aff50bb9bce7f064e2f465462aeb9af87a
BSorbus <BSorbus@gmail.com>
2022-03-28 15:40:47 +0200

    Correct esodes.rb

--- lib/esodes.rb
+++ lib/esodes.rb
@@ -71,9 +71,9 @@ module Esodes
   ACTION_EDIT = [ODNOWIENIE_BEZ_EGZAMINU, DUPLIKAT, WYMIANA, PROTOKOL]
 
   JRWA_L = [5430]
-  JRWA_M = [5431]
-  JRWA_R = [5432]
-  JRWA_ALL = [5430, 5431, 5432]
+  JRWA_M = [5411] # JRWA_M = [5431]
+  JRWA_R = [5412] # JRWA_R = [5432]
+  JRWA_ALL = [5430, 5411, 5412] #  JRWA_ALL = [5430, 5431, 5432]
 
   DEFAULT_SPOSOB_PRZESLANIA_IF_PROPOSAL = 78
 
@@ -185,9 +185,11 @@ module Esodes
     when 'L'
       5430
     when 'M'
-      5431
+      5411
+      # 5431
     when 'R'
-      5432
+      5412
+      # 5432
     else
       raise "Error value for Esodes::esod_matter_jrwa (#{service})!"
     end
